e0980116ffa12715fe8630b667aca84bf6ff23ad
Noemi Lovei <nila.noemi25@gmail.com>
2025-02-08 17:23:35 +0000

    v1 not working with categories

--- src/controllers/dashboard-controller.js
+++ src/controllers/dashboard-controller.js
@@ -1,8 +1,28 @@
+import { db } from "../models/db.js";
+
 export const dashboardController = {
-    index: {
-      handler: async function (request, h) {
-        return h.view("main");
-      },
+  index: {
+    handler: async function (request, h) {
+      const pois = await db.poiStore.getAllPois();
+      const viewData = {
+        title: "Point of Interest Dashboard",
+        pois: pois, 
+      };
+      return h.view("dashboard-view", viewData);
     },
-  };
-  
+  },
+
+  addPoi: {
+    handler: async function (request, h) {
+      const newPoi = {
+        name: request.payload.name,
+        description: request.payload.description,
+        category: request.payload.category,
+        location: request.payload.location,
+        image: request.payload.image, 
+      };
+      await db.poiStore.addPoi(newPoi);
+      return h.redirect("/dashboard");
+    },
+  },
+};
